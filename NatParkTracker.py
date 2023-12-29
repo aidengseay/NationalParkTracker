f52c74286b64f659203558d7ce81c9e503579708
aidenseay <ags392@nau.edu>
2023-12-28 20:51:07 -1000

    Added initial program and data files

---
+++ NatParkTracker.py
@@ -0,0 +1,47 @@
+################################################################################
+# NATIONAL PARK TRACKER
+# =====================
+# This program keeps track of all the national parks we have been to!
+# Created by Aiden and Stef, Winter 2023
+################################################################################
+# IMPORTS
+import NatParkUtility
+
+################################################################################
+# CONSTANTS
+
+################################################################################
+# MAIN FUNCTION
+def main():
+
+    # Read in the text file with national parks
+
+
+    # Ask user if they want to change the status on any park
+
+
+    # Display 
+
+    ... 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################################################################
+if __name__ == "__main__":
+    main()
